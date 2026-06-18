1e13ccb5ebede2d7e72241dc7373b0ea6bebe762
Sigithakim <sigithakim005@gmail.com>
2026-06-18 10:29:15 +0700

    refactoring

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -46,7 +46,7 @@ class _LoginPageState extends State<LoginPage> {
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    const Icon(Icons.person, size: 80, color: Colors.green),
+                    const Icon(Icons.person, size: 80, color: Color.fromARGB(255, 115, 159, 241)),
                     const SizedBox(height: 20),
 
                     // Username field
